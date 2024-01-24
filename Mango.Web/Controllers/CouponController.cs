38db77b7c30f48f3a8a1b3fd9314eba238adea0a
Flavio Oliveira <fmo@cesar.org.br>
2024-01-24 14:39:24 -0300

    Create add coupon feature

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Mango.Web.Controllers
 {
@@ -26,5 +27,25 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
-    }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+		public async Task<IActionResult> CouponCreate(CouponDto model)
+		{
+            if (ModelState.IsValid)
+            {
+				ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+				if (response != null && response.isSuccess)
+				{
+					return RedirectToAction(nameof(CouponIndex));
+				}
+			}
+			return View(model);
+		}
+	}
 }
