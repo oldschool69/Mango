859dafcf8fcc040d5f0c717ebe41a77c05c797a7
Flavio Oliveira <fmo@cesar.org.br>
2024-01-31 16:52:22 -0300

    Start implementing login feature

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -32,9 +32,18 @@ namespace Mango.Services.AuthAPI.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
         {
-            return Ok();
+            var loginResponse = await _authService.Login(model);
+
+            if (loginResponse.User is null)
+            {
+                _response.isSuccess = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+
+            return Ok(loginResponse);
         }
 
     }
